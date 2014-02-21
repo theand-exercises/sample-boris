f49992c760c26c76f21872443faa8fcdbd33e84b
Heesang Chae <theand@gmail.com>
2014-02-22 04:24:07 +0900

    boris-loader 사용법 드디어 익힘. .borisrc 사용법을 잘못 알았던 것. boris 인스턴스 생성하고, 로더에 인스턴스 넘기고, 기타 세팅해서 start하면 됨. boris 에서 namespace use는 안 되는 듯.

--- main.php
+++ main.php
@@ -2,6 +2,26 @@
 require_once 'vendor/autoload.php';
 
 use Goutte\Client;
+use Carbon\Carbon;
 
-$boris = new \Boris\Boris('myapp> ');
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\ErrorHandler;
+
+// create a log channel
+$log = new Logger('goutte');
+$log->pushHandler(new RotatingFileHandler('./logs/goutte.log', 0, Logger::WARNING));
+
+ErrorHandler::register($log);
+
+$boris = new \Boris\Boris("REPL>");
+
+\Boris\Loader\Loader::load($boris, array(
+    new \Boris\Loader\Provider\Composer(),
+));
+
+$boris->setPrompt('prompty> ');
+
+$boris->setLocal(array('log' => $log));
+
 $boris->start();
